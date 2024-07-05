46b165855ef52a7416185e70b87a12b35aeab9b7
Arthur Baldoqui <arthur.baldoqui.bergamo@gmail.com>
2024-07-04 21:42:48 -0300

    alterado post de calculo lambda

--- src/posts.ts
+++ src/posts.ts
@@ -1,7 +1,7 @@
 import { Post } from './types'; // Importe o tipo Post definido anteriormente
 
 const posts: Post[] = [
-    {uid:'calculo-lambda', title: 'Cálculo Lambda', filePath: 'posts/lambdaCalc/', content: null },
+    {uid:'calculo-lambda', title: 'Cálculo Lambda - Breve Introdução', filePath: 'posts/lambdaCalc/', content: null },
 ];
 
 export default posts;
